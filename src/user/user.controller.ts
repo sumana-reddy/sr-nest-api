fde77be30beb0ec20714ba2252a55c7f0b1a016b
sumanareddy <s538360@nwmissouri.edu>
2021-02-05 12:08:36 -0500

    modified

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,7 +3,7 @@ import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
-@Controller('user')
+/*@Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
@@ -19,7 +19,7 @@ export class UserController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.userService.findOne(+id);
+    return this.userService.findOne(id);
   }
 
   @Put(':id')
@@ -29,6 +29,46 @@ export class UserController {
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.userService.remove(+id);
+    return this.userService.remove(id);
   }
+}*/
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  // @Post()
+  // create(@Body() createUserDto: CreateUserDto) {
+  //   return this.userService.create(createUserDto);
+  // }
+
+  @Get()
+  // index():string{
+  //   return "This is the index page of user where all the users will be displayed.";
+  // }
+  index(): Promise<User[]> {
+    return this.userService.findAll();
+}
+    
+  
+
+  @Post('create')
+    async create(@Body() userData: User): Promise<any> {
+      return this.userService.create(userData);
+    }  
+   
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.userService.findOne(+id);
+  // }
+
+  // @Put(':id')
+  // update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+  //   return this.userService.update(+id, updateUserDto);
+  // }
+
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.userService.remove(+id);
+  // }
 }
